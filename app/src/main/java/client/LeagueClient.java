d964ce9de8b5b2e9944a19d5a215846266e5533e
NorbertLm <noobertlm@gmail.com>
2016-01-09 05:38:29 -0500

    Update LeagueClient.java

--- app/src/main/java/client/LeagueClient.java
+++ app/src/main/java/client/LeagueClient.java
@@ -11,7 +11,7 @@ import retrofit.http.GET;
  */
 public class LeagueClient {
 
-    public static final String API_URL="https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Norbert?api_key=ac30a2a7-5f32-4a54-821b-e82d7a5672d6";
+    public static final String API_URL="https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/Norbert?api_key=[];
 
     public interface LCS {
         @GET("/api/lol/na/v1.2/champion")
